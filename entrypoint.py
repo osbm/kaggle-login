249c7a3bccfeaa08318698ac25ed3bc9eba54fa2
osbm <osmanfbayram@gmail.com>
2022-08-17 00:38:44 +0300

    lets see

--- entrypoint.py
+++ entrypoint.py
@@ -4,3 +4,8 @@ print(os.getcwd())
 # print parent directory contents
 print(os.listdir('..'))
 print(os.listdir())
+
+
+# print current directory contents
+print(os.listdir('/home/runner/work/'))
+#print(os.listdir('/home/runner/work/'))
